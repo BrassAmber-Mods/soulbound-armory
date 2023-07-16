b48617a32765571e2f5299bc9107538f3e4eacdd
Muhammad <tjmnkrajyej@gmail.com>
2023-07-16 21:00:59 +0100

    Move top-level package to project's directory.

--- soulboundarmory/mixin/mixin/client/HeldItemRendererMixin.java
+++ soulboundarmory/mixin/mixin/client/HeldItemRendererMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.util.Arm;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.Vec3f;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
