7b1c61099123e7e064b8a3cec19ca0559488c84b
Muhammad <tjmnkrajyej@gmail.com>
2023-07-18 07:22:38 +0100

    Reverse floating item direction (#2).

--- soulboundarmory/mixin/mixin/client/GameRendererMixin.java
+++ soulboundarmory/mixin/mixin/client/GameRendererMixin.java
@@ -2,10 +2,12 @@ package soulboundarmory.mixin.mixin.client;
 
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.RaycastContext;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import soulboundarmory.component.soulbound.item.ItemComponent;
 import soulboundarmory.module.gui.widget.Widget;
 import soulboundarmory.skill.Skills;
@@ -24,5 +26,10 @@ abstract class GameRendererMixin {
 
 				return result == null ? Math2.square(reach) : result.squaredDistanceTo(player);
 			}).orElse(blockDistance) : blockDistance;
+	}
+
+	@Redirect(method = "renderFloatingItem", at = @At(value = "INVOKE", ordinal = 2, target = "Lnet/minecraft/util/math/MathHelper;sin(F)F"))
+	float reverseTransformingSoulboundItemDirection(float x) {
+		return MathHelper.abs(MathHelper.cos(x * 0.75F));
 	}
 }
