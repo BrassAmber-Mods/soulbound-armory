9dd418679c37591ac2db102eb279ccfa3c4e951a
Muhammad <tjmnkrajyej@gmail.com>
2023-07-18 07:30:22 +0100

    Hide floating item from non-FP views (#2).

--- soulboundarmory/mixin/mixin/client/GameRendererMixin.java
+++ soulboundarmory/mixin/mixin/client/GameRendererMixin.java
@@ -1,13 +1,16 @@
 package soulboundarmory.mixin.mixin.client;
 
+import net.minecraft.client.option.Perspective;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.RaycastContext;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import soulboundarmory.component.soulbound.item.ItemComponent;
 import soulboundarmory.module.gui.widget.Widget;
 import soulboundarmory.skill.Skills;
@@ -26,6 +29,13 @@ abstract class GameRendererMixin {
 
 				return result == null ? Math2.square(reach) : result.squaredDistanceTo(player);
 			}).orElse(blockDistance) : blockDistance;
+	}
+
+	@Inject(method = "renderFloatingItem", cancellable = true, at = @At("HEAD"))
+	void hideTransformingSoulboundItemFromNonFirstPersonViews(int scaledWidth, int scaledHeight, float tickDelta, CallbackInfo control) {
+		if (Widget.client.options.getPerspective() != Perspective.FIRST_PERSON) {
+			control.cancel();
+		}
 	}
 
 	@Redirect(method = "renderFloatingItem", at = @At(value = "INVOKE", ordinal = 2, target = "Lnet/minecraft/util/math/MathHelper;sin(F)F"))
